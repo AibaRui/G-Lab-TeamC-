6f2f3e02c042578fce5e3d3a3d913445baaada04
Kilimanjaro874 <vantan2022.228995@gmail.com>
2022-10-30 00:30:52 +0900

    トヨナガ：不要なスクリプト削除

--- Assets/Member!!!/Tominaga/Gimick/DieCutting/Script/DoorTemp.cs
+++ Assets/Member!!!/Tominaga/Gimick/DieCutting/Script/DoorTemp.cs
@@ -3,11 +3,14 @@ public class DoorTemp : MonoBehaviour
 {
     [SerializeField, Tooltip("DieCutting‚ð“o˜^")]
     DieCutting dc_;
+    private Vector3 moved;
+    bool is_move_ = true;
 
     void Update()
     {
         if (dc_.Is_complete)
         {
+            if(!is_move_) { return; }
             move();
         }
     }
@@ -15,6 +18,7 @@ public class DoorTemp : MonoBehaviour
     void move()
     {
         transform.position += new Vector3(0, 0.1f, 0);
-
+        moved += new Vector3(0, 0.1f, 0);
+        if(moved.magnitude > 5) { is_move_ = false; }
     }
 }
