d595544cbc3216fcd888953f2111acd5bad234a8
Kilimanjaro874 <vantan2022.228995@gmail.com>
2022-11-06 22:36:26 +0900

    toyonaga->パーティクルシステム修正

--- Assets/Member!!!/Toyonaga/Gimick/ParticleSystem/Script/ParticleMove.cs
+++ Assets/Member!!!/Toyonaga/Gimick/ParticleSystem/Script/ParticleMove.cs
@@ -5,8 +5,12 @@ public class ParticleMove : MonoBehaviour
     [SerializeField]
     private Rigidbody2D rb2D_;      // リジッドボディ取得
     // ---- 制御パラメータ ---- //
-    [SerializeField, Tooltip("速度上限を指定")]
+    [Header("速度上限"), SerializeField]
     float vel_max_ = 5f;                 // 速度制限
+    [Header("速度ランダム上限 < vel_max"), SerializeField]
+    float vel_rand_max_ = 3.5f;
+    [Header("速度ランダムの度合(0-1:velmax"), SerializeField]
+    float vel_rand_coff_ = 0.5f;
     [SerializeField, Tooltip("高いほど直線敵に目標位置へ移動,質量設定も重要")]
     private float kp_ = 5f;              // P制御係数
     [SerializeField, Tooltip("高いほど精密に目標位置へ移動、質量設定も重要")]
@@ -21,15 +25,15 @@ public class ParticleMove : MonoBehaviour
     private void Start()
     {
         // ---- 初期化処理 ---- //
-        //target_pos_ = new Vector3(0, 0, 0);
         rb2D_ = GetComponent<Rigidbody2D>();
+        vel_max_ += Random.Range(-vel_rand_max_ * vel_rand_coff_, vel_rand_max_ * vel_rand_coff_);
     }
 
     private void FixedUpdate()
     {
         // ---- パーティクルを目標位置(target_pos_)へ制御 ---- //
-         rb2D_.AddForce(MoveCont(ref target_pos_, Time.deltaTime));
-        if(rb2D_.velocity.sqrMagnitude > vel_max_ * vel_max_)   // ルート計算は重いので、判定式では避ける
+        rb2D_.AddForce(MoveCont(ref target_pos_, Time.deltaTime));
+        if (rb2D_.velocity.sqrMagnitude > vel_max_ * vel_max_)   // ルート計算は重いので、判定式では避ける
         {
             rb2D_.velocity = rb2D_.velocity.normalized * vel_max_;  // 速度制限
         }
@@ -38,15 +42,15 @@ public class ParticleMove : MonoBehaviour
     private Vector3 MoveCont(ref Vector3 target, float delta_time)
     {
         // ---- パーティクルが目標位置へ向うための力ベクトルを返す ---- //
-        Vector3 pow = new Vector3 (0, 0, 0);
+        Vector3 pow = new Vector3(0, 0, 0);
         Vector3 posError = target - transform.position;     // 誤差取得
         // --- 制御入力生成 --- //
         integral_error_ = (posError + pre_error_) / 2 * delta_time;
-        pow =   kp_ * posError +
+        pow = kp_ * posError +
                 ki_ * integral_error_ +
                 kd_ * (posError - pre_error_) / delta_time;
         pre_error_ = posError;      // 1 delta_time_前の位置誤差を格納しておく
-        
+
         return pow;
     }
 
--- Assets/Member!!!/Toyonaga/Gimick/ParticleSystem/Script/Test_Player_move.cs
+++ Assets/Member!!!/Toyonaga/Gimick/ParticleSystem/Script/Test_Player_move.cs
@@ -5,7 +5,6 @@ public class Test_Player_move : MonoBehaviour
 
     // ---- PartcicleSystemの使用例 ---- //
     public Rigidbody2D rb2D;
-    public ParticleController Pc; 
     // Start is called before the first frame update
     void Start()
     {
@@ -20,13 +19,11 @@ public class Test_Player_move : MonoBehaviour
 
         // --- ParticleSystem:以下の２パターンで使用 --- //
         if (Input.GetKeyDown(KeyCode.Space)){
-            // -- 指定のオーラのモード（円 or 四角(前(transform.right方向 , 上(tranform.up方向, 後、 下))で状態変化 -- //
-            Pc.SetAuraChange(ParticleController.mode_.circle);      
+         
         }
         if (Input.GetKeyDown(KeyCode.Return))
         {
-            // -- 一度呼び出す度、オーラのモードを円、四角(前、上、後、下）、円..の順で変更 -- //
-            Pc.SetAuraChangeRow();
+         
         }
 
     }
