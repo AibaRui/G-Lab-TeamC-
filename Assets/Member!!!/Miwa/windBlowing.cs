b7dbc74ae1d0649101771d657a339262aa67a8b7
Hikari384 <vga.niwak13@gmail.com>
2022-11-15 17:32:53 +0900

    壁等に近づいているときに、風の影響を受けないよう、変更しました。 現状、タグでの判別を行っています。

--- Assets/Member!!!/Miwa/windBlowing.cs
+++ Assets/Member!!!/Miwa/windBlowing.cs
@@ -11,14 +11,15 @@ public class windBlowing : MonoBehaviour
     [SerializeField] private float playerRayLength = 5;
     float blowingTimer;
     bool isBlow;
+    bool p1Hide;
+    bool p2Hide;
     GameObject p1;
     GameObject p2;
     Rigidbody2D p1rb;
     Rigidbody2D p2rb;
     Vector2 p1Pos;
     Vector2 p2Pos;
-    Vector2 p1Dir;
-    Vector2 p2Dir;
+    Vector2 playerDir;
     Ray2D p1Ray;
     Ray2D p2Ray;
     void Start()
@@ -27,38 +28,53 @@ public class windBlowing : MonoBehaviour
         p2 = GameObject.Find(player2);
         p1rb = p1.GetComponent<Rigidbody2D>();
         p2rb = p2.GetComponent<Rigidbody2D>();
+        playerDir = new Vector2(playerRayLength, 0);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        p1Pos = p1.transform.position;
+        p1Pos = p1.transform.position ;
         p2Pos = p2.transform.position;
-        p1Dir = new Vector2(p1.transform.position.x + playerRayLength, p1.transform.position.y);
-        p2Dir = new Vector2(p2.transform.position.x + playerRayLength, p2.transform.position.y);
-        p1Ray = new Ray2D(p1Pos, p1Dir);
-        p2Ray = new Ray2D(p2Pos, p2Dir);
-        if (isBlow)     
+        p1Ray = new Ray2D(p1Pos, playerDir);
+        p2Ray = new Ray2D(p2Pos, playerDir);
+        RaycastHit2D p1hit = Physics2D.Raycast(p1Pos,Vector2.right, playerRayLength);
+        RaycastHit2D p2hit = Physics2D.Raycast(p2Pos,Vector2.right, playerRayLength);
+        if(p1hit.collider != null && p1hit.collider.gameObject.tag != "player")
+        {
+            p1Hide = true;
+        }
+        else { p1Hide = false; }
+        if (p2hit.collider != null && p2hit.collider.gameObject.tag != "player")
+        {
+            p2Hide = true;
+        }
+        else { p2Hide= false; }
+        if (isBlow)
         {
             blowingTimer += Time.deltaTime;
             if (blowingTimer < blowTime)
             {
-                p1rb.AddForce(new Vector2(-wPower, 0));
-                p2rb.AddForce(new Vector2(-wPower, 0));
+                if (p1Hide == false) { p1rb.AddForce(new Vector2(-wPower, 0)); }
+                if (p2Hide == false) { p2rb.AddForce(new Vector2(-wPower, 0)); }
             }
             else
             {
                 isBlow = false;
             }
+            
         }
         //デバッグ用
         if (Input.GetKeyDown(KeyCode.Space))        
         {
             blowing();
         }
-        Debug.DrawRay(p1Pos, p1Dir*1, Color.red);
-        Debug.DrawRay(p2Pos, p2Dir*1, Color.red);
+        
+        Debug.DrawRay(p1Pos, playerDir *1, Color.red);
+        Debug.DrawRay(p2Pos, playerDir *1, Color.red);
+        Debug.Log($"p1,{p1Hide}");
+        Debug.Log(p2Hide);
     }
 
     public void blowing()   //風が吹くタイマーセット
@@ -67,4 +83,5 @@ public class windBlowing : MonoBehaviour
         isBlow = true;   
         
     }
+
 }
