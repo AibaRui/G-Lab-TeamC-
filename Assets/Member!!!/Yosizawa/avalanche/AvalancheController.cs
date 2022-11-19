6dd98fc7de4e91ba5a6c35f07a877018b63faf15
tatsupiko <identity2022@outlook.jp>
2022-11-19 17:57:59 +0900

    雪崩にPauseResumeを追加

--- Assets/Member!!!/Yosizawa/avalanche/AvalancheController.cs
+++ Assets/Member!!!/Yosizawa/avalanche/AvalancheController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
-public class AvalancheController : MonoBehaviour
+public class AvalancheController : GimickBase
 {
     [Header("ê·ïˆÇÃêiçsë¨ìx")]
     [SerializeField] float _speed = 1.0f;
     Rigidbody2D _rb;
+    Vector2 _saveVelocity;
+
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
@@ -25,5 +27,26 @@ public class AvalancheController : MonoBehaviour
         {
             collision.gameObject.SetActive(false);
         }
+    }
+
+    public override void GameOverPause()
+    {
+        _saveVelocity = _rb.velocity;
+        _rb.Sleep();
+        _rb.simulated = false;
+    }
+
+    public override void Pause()
+    {
+        _saveVelocity = _rb.velocity;
+        _rb.Sleep();
+        _rb.simulated = false;
+    }
+
+    public override void Resume()
+    {
+        _rb.simulated = true;
+        _rb.WakeUp();
+        _rb.velocity = _saveVelocity;
     }
 }
