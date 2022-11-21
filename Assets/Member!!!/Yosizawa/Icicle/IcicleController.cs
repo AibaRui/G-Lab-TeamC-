71d24d20b7c3a5ceb9e869d76f38729c907bcc37
tatsupiko <identity2022@outlook.jp>
2022-11-21 15:43:08 +0900

    全てのギミックにGimickBaseを継承

--- Assets/Member!!!/Yosizawa/Icicle/IcicleController.cs
+++ Assets/Member!!!/Yosizawa/Icicle/IcicleController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
-public class IcicleController : GimickBase
+class IcicleController : GimickBase
 {
     [Header("落下速度")]
     [SerializeField] float _fallSpeed = 1.0f;
@@ -13,8 +13,6 @@ public class IcicleController : GimickBase
     [SerializeField,Tooltip("ゲームオブジェクトの scale の最小")] float _minSize = 2.0f;
     [Header("このゲームオブジェクトの最大の大きさ")]
     [SerializeField, Tooltip("ゲームオブジェクトの scale の最大")] float _maxSize = 6.0f;
-    [Header("GameObject消失時に再生するAnimetion")]
-    [SerializeField] Animator _onDestroyAnimation = default;
     [Header("Playerを検知する光線を 表示 or 非表示")]
     [SerializeField,Tooltip("Rayの表示・非表示の切り替え")] bool _isGizmo = false;
     [Header("Playerを検知する光線を飛ばす方向")]
@@ -25,7 +23,9 @@ public class IcicleController : GimickBase
     Vector2 _dir = Vector2.zero;
     /// <summary>Rayを飛ばして当たったcolliderの情報</summary>
     RaycastHit2D _hit;
-    Rigidbody2D _rb;
+    /// <summary>一定の大きさになったかを判定するフラグ</summary>
+    bool _isScale =false;
+    Rigidbody2D _rb1;
     float _saveGravityScale;
     float _saveAngularVelocity;
     Vector2 _saveVelocity;
@@ -33,9 +33,8 @@ public class IcicleController : GimickBase
     void Start()
     {
         _magnification /= 100f;  //整数だと倍率が高すぎるので、あらかじめ低くしておく
-        _rb = GetComponent<Rigidbody2D>();
-        _rb.gravityScale = 0;
-        Debug.LogWarning("AnimatorのTransitionにAnimationSpeedのパラメータを設定しましたか？");
+        _rb1 = GetComponent<Rigidbody2D>();
+        _rb1.gravityScale = 0;
     }
 
     void Update()
@@ -45,7 +44,7 @@ public class IcicleController : GimickBase
 
         if(_hit.collider)  //RayがPlayerに当たった時の処理
         {
-            _rb.gravityScale = _fallSpeed;
+            _rb1.gravityScale = _fallSpeed;
         }
     }
 
@@ -80,36 +79,55 @@ public class IcicleController : GimickBase
             localScale.x += _magnification;
             localScale.y += _magnification;
             transform.localScale = localScale;
+
+            if(transform.localScale.x >= _maxSize && transform.localScale.y >= _maxSize)
+            {
+                _isScale = true;
+            }
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Ground")
+        {
+            if(_isScale)
+            {
+                _rb1.constraints = RigidbodyConstraints2D.FreezeAll;
+                //_rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+                Debug.Log("Alien");
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
         }
     }
 
     public override void GameOverPause()
     {
-        _saveGravityScale = _rb.gravityScale;
-        _saveAngularVelocity = _rb.angularVelocity;
-        _saveVelocity = _rb.velocity;
-        _rb.Sleep();
-        _rb.simulated = false;
-        //_onDestroyAnimation.speed = 0;
+        _saveGravityScale = _rb1.gravityScale;
+        _saveAngularVelocity = _rb1.angularVelocity;
+        _saveVelocity = _rb1.velocity;
+        _rb1.Sleep();
+        _rb1.simulated = false;
     }
 
     public override void Pause()
     {
-        _saveGravityScale = _rb.gravityScale;
-        _saveAngularVelocity = _rb.angularVelocity;
-        _saveVelocity = _rb.velocity;
-        _rb.Sleep();
-        _rb.simulated = false;
-        //_onDestroyAnimation.speed = 0;
+        _saveGravityScale = _rb1.gravityScale;
+        _saveAngularVelocity = _rb1.angularVelocity;
+        _saveVelocity = _rb1.velocity;
+        _rb1.Sleep();
+        _rb1.simulated = false;
     }
 
     public override void Resume()
     {
-        _rb.simulated = true;
-        _rb.WakeUp();
-        _rb.gravityScale = _saveGravityScale;
-        _rb.angularVelocity = _saveAngularVelocity;
-        _rb.velocity = _saveVelocity;
-        //_onDestroyAnimation.speed = 1;
+        _rb1.simulated = true;
+        _rb1.WakeUp();
+        _rb1.gravityScale = _saveGravityScale;
+        _rb1.angularVelocity = _saveAngularVelocity;
+        _rb1.velocity = _saveVelocity;
     }
 }
