4a2b8dd8604bb96df878e4adeecaf4e8811f7354
hanaryou0730 <hanaryo730@gmail.com>
2022-12-01 05:24:33 +0900

    ランキング機能実装中・・・

--- Assets/Member!!!/Morisima/Script/Forme.cs
+++ Assets/Member!!!/Morisima/Script/Forme.cs
@@ -1,18 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Forme : MonoBehaviour
 {
+    //InputFieldを格納するための変数
+    InputField inputField;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //InputFieldコンポーネントを取得
+        inputField = GameObject.Find("InputField").GetComponent<InputField>();
+    }
+
+
+    //入力された名前情報を読み取ってコンソールに出力する関数
+    public void OnClick()
+    {
+        //InputFieldからテキスト情報を取得する
+        string name = inputField.text;
+        Debug.Log(name);
+
+        //入力フォームのテキストを空にする
+        inputField.text = "";
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+    
+
+
 }
