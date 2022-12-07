d58343e04d3b44d6a9beb6ced8b4bdb088f00061
tatsupiko <identity2022@outlook.jp>
2022-12-07 14:51:00 +0900

    吹雪を発生させる Blizzard コンポーネントを修正＆改善

--- Assets/Member!!!/Yosizawa/Blizzard/blizzardController.cs
+++ Assets/Member!!!/Yosizawa/Blizzard/blizzardController.cs
@@ -5,10 +5,17 @@ using UnityEngine.UI;
 using DG.Tweening;
 
 [RequireComponent(typeof(AudioSource))]
+
 public class blizzardController : MonoBehaviour
 {
-    [SerializeField, Tooltip("blizzardを起こすために使うpanel")]
+    [SerializeField, Tooltip("吹雪を起こすために使うpanel")]
     private Image _blizzardPanel = null;
+    /// <summary>吹雪を起こすために使うpanelのprefab</summary>
+    private Image _panelPrefab;
+    [SerializeField, Tooltip("吹雪のパーティクル")] 
+    private ParticleSystem _blizzard = null;
+    /// <summary>吹雪のパーティクルのprefab</summary>
+    private ParticleSystem _particlePrefab;
     [SerializeField, Range(1f, 10f), Tooltip("どのくらいの時間吹雪を起こすか")]
     private float _stormTime;
     [SerializeField, Range(0f, 1f), Tooltip("blizzardを起こすときに使うpanelのalpha値")]
@@ -18,15 +25,21 @@ public class blizzardController : MonoBehaviour
 
     private void Start()
     {
+        GameObject particle;
+        //吹雪のパーティクルをカメラの子オブジェクトにする
+        GameObject camera = GameObject.Find("Main Camera");
+        particle = Instantiate(_blizzard.gameObject, camera.transform);
+        _particlePrefab = particle.GetComponent<ParticleSystem>();
+
+        GameObject panel;
+        //吹雪のパネルをキャンバスの子オブジェクトにする
         GameObject canvas = GameObject.Find("Canvas");
-        GameObject blizzardPanel = Instantiate(_blizzardPanel.gameObject, canvas.transform);
-        //blizzardPanel.transform.SetParent(canvas.transform);
-        //blizzardPanel.GetComponent<RectTransform>().localPosition = Vector2.zero;
-        //_blizzardPanel.enabled = false;
-        _blizzardPanel.DOFade(0f, 0f);  //念のためここでGameObjectを透明にしておく
-        _audio.DOFade(0f, 0f);
+        panel = Instantiate(_blizzardPanel.gameObject, canvas.transform);
+        _panelPrefab = panel.GetComponent<Image>();
+        _blizzardPanel.DOFade(0f, 0f);  //ここでGameObjectを透明にしておく
         if (_blizzardPanel is null)
             Debug.LogWarning("吹雪を起こしたいのですが、GameObjectがセットされていません。");
+
         _audio = GetComponent<AudioSource>();
     }
 
@@ -36,14 +49,21 @@ public class blizzardController : MonoBehaviour
 
         if (collision.gameObject.tag is "Player1" or "Player2")
         {
-            //_blizzardPanel.enabled = true;
-            _blizzardPanel.DOFade(_alpha, random);
+            _panelPrefab.DOFade(_alpha, random);
+            _particlePrefab.Play();
             _audio.Play();
             _audio.DOFade(1f, random);
+            Debug.Log("Clear!");
             DOVirtual.DelayedCall(_stormTime, () =>
             {
-                _blizzardPanel.DOFade(0f, random);
-                _audio.DOFade(0f, random).OnComplete(() => _audio.Stop());
+                Debug.Log("Great!");
+                _panelPrefab.DOFade(0f, random);
+                _audio.DOFade(0f, random).OnComplete(() =>
+                {
+                    _audio.Stop();
+                    _particlePrefab.Stop();
+                    Debug.Log("Perfect!");
+                });
             }, false);
         }
     }
