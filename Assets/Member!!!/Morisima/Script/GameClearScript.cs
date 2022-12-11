cc0168cac4cb690408f91df079747569f715b248
DESKTOP-T8MHPRI\vantan <kou8866m@gmail.com>
2022-12-11 11:02:53 +0900

    UI作業

--- Assets/Member!!!/Morisima/Script/GameClearScript.cs
+++ Assets/Member!!!/Morisima/Script/GameClearScript.cs
@@ -29,40 +29,40 @@ public class GameClearScript : MonoBehaviour
     void Judge()
     {
         // Ornament1 取得数に応じたオーナメントの表示
-        if (Ornament.Ornament1Score >= 1) { Ornament1Image[0].gameObject.SetActive(true); }
-        if (Ornament.Ornament1Score >= 2) { Ornament1Image[1].gameObject.SetActive(true); }
-        if (Ornament.Ornament1Score >= 3) { Ornament1Image[2].gameObject.SetActive(true); }
-        if (Ornament.Ornament1Score >= 4) { Ornament1Image[3].gameObject.SetActive(true); }
-        if (Ornament.Ornament1Score >= 5) { Ornament1Image[4].gameObject.SetActive(true); }
-        if (Ornament.Ornament1Score >= 6) { Ornament1Image[5].gameObject.SetActive(true); }
-        if (Ornament.Ornament1Score >= 7) { Ornament1Image[6].gameObject.SetActive(true); }
-        if (Ornament.Ornament1Score >= 8) { Ornament1Image[7].gameObject.SetActive(true); }
-        if (Ornament.Ornament1Score >= 9) { Ornament1Image[8].gameObject.SetActive(true); }
-        if (Ornament.Ornament1Score >= 10) { Ornament1Image[9].gameObject.SetActive(true); }
+        if (Ornament1.Ornament1Score >= 1) { Ornament1Image[0].gameObject.SetActive(true); }
+        if (Ornament1.Ornament1Score >= 2) { Ornament1Image[1].gameObject.SetActive(true); }
+        if (Ornament1.Ornament1Score >= 3) { Ornament1Image[2].gameObject.SetActive(true); }
+        if (Ornament1.Ornament1Score >= 4) { Ornament1Image[3].gameObject.SetActive(true); }
+        if (Ornament1.Ornament1Score >= 5) { Ornament1Image[4].gameObject.SetActive(true); }
+        if (Ornament1.Ornament1Score >= 6) { Ornament1Image[5].gameObject.SetActive(true); }
+        if (Ornament1.Ornament1Score >= 7) { Ornament1Image[6].gameObject.SetActive(true); }
+        if (Ornament1.Ornament1Score >= 8) { Ornament1Image[7].gameObject.SetActive(true); }
+        if (Ornament1.Ornament1Score >= 9) { Ornament1Image[8].gameObject.SetActive(true); }
+        if (Ornament1.Ornament1Score >= 10) { Ornament1Image[9].gameObject.SetActive(true); }
 
         // Ornament2 取得数に応じたオーナメントの表示
-        if (Ornament.Ornament2Score >= 1) { Ornament2Image[0].gameObject.SetActive(true); }
-        if (Ornament.Ornament2Score >= 2) { Ornament2Image[1].gameObject.SetActive(true); }
-        if (Ornament.Ornament2Score >= 3) { Ornament2Image[2].gameObject.SetActive(true); }
-        if (Ornament.Ornament2Score >= 4) { Ornament2Image[3].gameObject.SetActive(true); }
-        if (Ornament.Ornament2Score >= 5) { Ornament2Image[4].gameObject.SetActive(true); }
-        if (Ornament.Ornament2Score >= 6) { Ornament2Image[5].gameObject.SetActive(true); }
-        if (Ornament.Ornament2Score >= 7) { Ornament2Image[6].gameObject.SetActive(true); }
-        if (Ornament.Ornament2Score >= 8) { Ornament2Image[7].gameObject.SetActive(true); }
-        if (Ornament.Ornament2Score >= 9) { Ornament2Image[8].gameObject.SetActive(true); }
-        if (Ornament.Ornament2Score >= 10) { Ornament2Image[9].gameObject.SetActive(true); }
+        if (Ornament2.Ornament2Score >= 1) { Ornament2Image[0].gameObject.SetActive(true); }
+        if (Ornament2.Ornament2Score >= 2) { Ornament2Image[1].gameObject.SetActive(true); }
+        if (Ornament2.Ornament2Score >= 3) { Ornament2Image[2].gameObject.SetActive(true); }
+        if (Ornament2.Ornament2Score >= 4) { Ornament2Image[3].gameObject.SetActive(true); }
+        if (Ornament2.Ornament2Score >= 5) { Ornament2Image[4].gameObject.SetActive(true); }
+        if (Ornament2.Ornament2Score >= 6) { Ornament2Image[5].gameObject.SetActive(true); }
+        if (Ornament2.Ornament2Score >= 7) { Ornament2Image[6].gameObject.SetActive(true); }
+        if (Ornament2.Ornament2Score >= 8) { Ornament2Image[7].gameObject.SetActive(true); }
+        if (Ornament2.Ornament2Score >= 9) { Ornament2Image[8].gameObject.SetActive(true); }
+        if (Ornament2.Ornament2Score >= 10) { Ornament2Image[9].gameObject.SetActive(true); }
 
         // Ornament3 取得数に応じたオーナメントの表示
-        if (Ornament.Ornament3Score >= 1) { Ornament3Image[0].gameObject.SetActive(true); }
-        if (Ornament.Ornament3Score >= 2) { Ornament3Image[1].gameObject.SetActive(true); }
-        if (Ornament.Ornament3Score >= 3) { Ornament3Image[2].gameObject.SetActive(true); }
-        if (Ornament.Ornament3Score >= 4) { Ornament3Image[3].gameObject.SetActive(true); }
-        if (Ornament.Ornament3Score >= 5) { Ornament3Image[4].gameObject.SetActive(true); }
-        if (Ornament.Ornament3Score >= 6) { Ornament3Image[5].gameObject.SetActive(true); }
-        if (Ornament.Ornament3Score >= 7) { Ornament3Image[6].gameObject.SetActive(true); }
-        if (Ornament.Ornament3Score >= 8) { Ornament3Image[7].gameObject.SetActive(true); }
-        if (Ornament.Ornament3Score >= 9) { Ornament3Image[8].gameObject.SetActive(true); }
-        if (Ornament.Ornament3Score >= 10) { Ornament3Image[9].gameObject.SetActive(true); }
+        if (Ornament3.Ornament3Score >= 1) { Ornament3Image[0].gameObject.SetActive(true); }
+        if (Ornament3.Ornament3Score >= 2) { Ornament3Image[1].gameObject.SetActive(true); }
+        if (Ornament3.Ornament3Score >= 3) { Ornament3Image[2].gameObject.SetActive(true); }
+        if (Ornament3.Ornament3Score >= 4) { Ornament3Image[3].gameObject.SetActive(true); }
+        if (Ornament3.Ornament3Score >= 5) { Ornament3Image[4].gameObject.SetActive(true); }
+        if (Ornament3.Ornament3Score >= 6) { Ornament3Image[5].gameObject.SetActive(true); }
+        if (Ornament3.Ornament3Score >= 7) { Ornament3Image[6].gameObject.SetActive(true); }
+        if (Ornament3.Ornament3Score >= 8) { Ornament3Image[7].gameObject.SetActive(true); }
+        if (Ornament3.Ornament3Score >= 9) { Ornament3Image[8].gameObject.SetActive(true); }
+        if (Ornament3.Ornament3Score >= 10) { Ornament3Image[9].gameObject.SetActive(true); }
     }
 
 }
--- Assets/Member!!!/Morisima/Script/Ornament2.cs
+++ Assets/Member!!!/Morisima/Script/Ornament2.cs
@@ -3,37 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Ornament : MonoBehaviour
+public class Ornament2 : MonoBehaviour
 {
-    // Ornament ¯‚Ì‰ÁZ—p•Ï”
-    public static int Ornament1Score = 0;
-
     // Ornament ¼‚Ú‚Á‚­‚è‚Ì‰ÁZ—p•Ï”
     public static int Ornament2Score = 0;
 
-    // Ornament •A‚Ì‰ÁZ—p•Ï”
-    public static int Ornament3Score = 0;
-
-    // ƒcƒŠ[—p‚Ì Ornament ‰ÁZ—p•Ï”
-    public static int Ornament4Score = 0;
-
     void Start()
     {
-        Ornament1Score = 0;
         Ornament2Score = 0;
-        Ornament3Score = 0;
-        Ornament4Score = 0;
     }
-
-    void Update()
-    {
-        
-    }
-
 
     // Player ‚ª Ornament ‚É“–‚½‚Á‚½Á‚¦‚é
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        Ornament2Score += 1;
         Destroy(this.gameObject);
     }
 }
