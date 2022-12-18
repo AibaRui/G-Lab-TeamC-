aee5e568d538131ec7a4d606daaafe8787ddc1e2
DESKTOP-T8MHPRI\vantan <kou8866m@gmail.com>
2022-12-18 14:34:27 +0900

    UI作業

--- Assets/Member!!!/Morisima/Script/Ornament/Ornament8.cs
+++ Assets/Member!!!/Morisima/Script/Ornament/Ornament8.cs
@@ -3,20 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Ornament1 : MonoBehaviour
+public class Ornament8 : MonoBehaviour
 {
-    // Ornament ¯‚Ì‰ÁZ—p•Ï”
-    public static int Ornament1Score = 0;
+    // Ornament á‚ÌŒ‹»‚Ì‰ÁZ—p•Ï”
+    public static int Ornament8Score = 0;
 
     void Start()
     {
-        Ornament1Score = 0;
+        Ornament8Score = 0;
     }
 
     // Player ‚ª Ornament ‚É“–‚½‚Á‚½Á‚¦‚é
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Ornament1Score += 1;
+        Ornament8Score += 1;
         Destroy(this.gameObject);
     }
 }
